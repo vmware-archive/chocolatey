9103bca7cbac15519c34e0d7e0b897bc8cbd5031
Florent Biville <fbiville@pivotal.io>
2019-02-21 16:37:31 +0100

    Package riff 0.2.0

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  PackageName   = $env:ChocolateyPackageName
+  UnzipLocation = $toolsDir
+  Url64bit      = "https://github.com/projectriff/riff/releases/download/v$($env:ChocolateyPackageVersion)/riff-windows-amd64.zip"
+  Checksum64    = '03620d3c83493ab57b9a3982a785f74a8e89b0af8450e728d2761279456e8285'
+  ChecksumType64= 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+Remove-Item "$toolsDir\riff-windows*.zip"
+Install-BinFile -Name "riff" -Path "$toolsDir\riff.exe"
