fba5ba6e4c79fc905b4ba66681df139c908fdbc2
@fbiville <florent.biville@gmail.com>
2019-02-25 10:46:10 +0100

    Remove explicit binary install and uninstall
    and fix metadata as suggested by Chocolatey reviewer

--- tools/chocolateyUninstall.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference = 'Stop'
-
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-Remove-BinFile -Name "riff" -Path "$toolsDir\riff.exe"
--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,11 +5,10 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   PackageName   = $env:ChocolateyPackageName
   UnzipLocation = $toolsDir
-  Url64bit      = "https://github.com/projectriff/riff/releases/download/v$($env:ChocolateyPackageVersion)/riff-windows-amd64.zip"
+  Url64bit      = "https://github.com/projectriff/riff/releases/download/v0.2.0/riff-windows-amd64.zip"
   Checksum64    = '03620d3c83493ab57b9a3982a785f74a8e89b0af8450e728d2761279456e8285'
   ChecksumType64= 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
 Remove-Item "$toolsDir\riff-windows*.zip"
-Install-BinFile -Name "riff" -Path "$toolsDir\riff.exe"
