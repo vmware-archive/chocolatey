1d485f7de29f10ffff4b5affa7e2a86cbe14820d
trisberg <trisberg@pivotal.io>
2019-05-21 11:55:31 -0400

    Release 0.3.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   PackageName   = $env:ChocolateyPackageName
   UnzipLocation = $toolsDir
-  Url64bit      = 'https://github.com/projectriff/riff/releases/download/v0.3.0/riff-windows-amd64.zip'
-  Checksum64    = '568ef7ae1e692c81f33a74083dcfadf137d594593ed5e286438df98c46a465d2'
+  Url64bit      = 'https://github.com/projectriff/riff/releases/download/v0.3.1/riff-windows-amd64.zip'
+  Checksum64    = 'a6bc229357f8e8d36ec5204158cbaa2a162fe34a63c16d7ddead56f848e70f23'
   ChecksumType64= 'sha256'
 }
 
