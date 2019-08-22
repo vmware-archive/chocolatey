398784801554ff77031f7376b36ad6a01a72845b
Florent Biville <fbiville@pivotal.io>
2019-08-22 10:42:32 +0200

    Point to Github release 0.4.0's ZIP instead

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   PackageName   = $env:ChocolateyPackageName
   UnzipLocation = $toolsDir
-  Url64bit      = 'https://storage.googleapis.com/projectriff/riff-cli/releases/v0.4.0/riff-windows-amd64.zip'
+  Url64bit      = 'https://github.com/projectriff/riff/releases/download/v0.4.0/riff-windows-amd64.zip'
   Checksum64    = '2c8f288706c6f19eaa3c0f577e2daf3ef60d839879e7eb5e4be728cd124d9dba'
   ChecksumType64= 'sha256'
 }
